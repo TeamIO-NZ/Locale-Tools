9a3bc586342c731f59b83d02a16b6d0db1391ead
Joe van der Zwet <me@joezwet.me>
2019-09-08 12:15:41 +1200

    :boom: Replace XML with JSON and rewrite tool

--- EventfindaScraper.py
+++ EventfindaScraper.py
@@ -1,47 +1,24 @@
 import csv
-import xml.sax
+import json
 import os
-import httplib2
 
-rows = [["Title", "Description", "Where", "Time", "Icon"]]
-
-class EventHandler( xml.sax.ContentHandler ):
-    def __init__(self):
-        self.CurrentData = ""
-        self.title = ""
-        self.description = ""
-        self.where = ""
-        self.time = ""
-        self.icon_url = ""
-
-    def startElement(self, tag, attributes):
-        self.CurrentData = tag
-        if tag == "enclosure":
-            url = attributes['url']
-            self.icon_url = url
-
-    def endElement(self, tag):
-        if tag == "item":
-            rows.append([self.title, self.description, self.where, self.time, self.icon_url])
-        self.CurrentData = ""
-
-    def characters(self, content):
-        if self.CurrentData == "title":
-            self.title = content
-        elif self.CurrentData == "description":
-            self.description = content.split(" ...")[0] + "..."
-        elif self.CurrentData == "content":
-            self.time = content[:-(len(content) - (len(content.split(', ')[len(content.split(', '))-1]) + (len(content.split(', ') * 2))))]
-            self.where = content.split(", ")[len(content.split(', '))-1].replace("<br/>", " ")
-
-
+rows = [["Title", "Description", "Date", "Start Time", "End Time", "Location", "Icon"]]
 
 if ( __name__ == "__main__"):
-    content = open('eventfinda.xml', 'r')
-    parser = xml.sax.make_parser()
-    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
-    parser.setContentHandler(EventHandler())
-    parser.parse(content)
+    file = open('events.json', 'r')
+    content = json.loads(file.read())
+
+    for e in content['events']:
+        rows.append([
+            e['title'], 
+            e['description'], 
+            e['date'], 
+            e['start_time'], 
+            e['end_time'], 
+            e['location'], 
+            e['icon']
+            ])
+    file.close()
 
     with open("export/eventfinda.csv", "wb") as eventCsv:
         writer = csv.writer(eventCsv)
